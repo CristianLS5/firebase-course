f42ebb5dcc40f3ec101c086cc7a231b8bbec0d18
CNLZgft <cristian.lopez@gft.com>
2023-09-19 16:13:02 +0200

    converting the FireStone query results to the Domain Model required

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,27 @@
+import { Inject } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { map } from "rxjs/operators";
+import { convertSnaps } from "./db-utils";
+
+@Inject({
+  providedIn: "root",
+})
+export class CoursesService {
+  constructor(private db: AngularFirestore) {}
+
+  //se definen los metódos usando la API pública  de FireStore para nuestro servicio
+
+  loadCoursesByCategory(category: string): Observable<Course[]> {
+    //FireStore  devuelve valores de tipo "QuerySnapshot" y noostros queremos valores
+    //que sean del tipo de modelo "Course"
+    return this.db
+      .collection("courses", (ref) =>
+        //array-contains permite realizar queries en casos donde el contenido sea un array
+        ref.where("categories", "array-contains", category).orderBy("seqNo")
+      )
+      .get()
+      .pipe(map((result) => convertSnaps<Course>(result)));
+  }
+}
