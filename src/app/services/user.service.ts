e4539dc730e5063f5807f7678d6144e66a4bc306
CNLZgft <cristian.lopez@gft.com>
2023-09-25 18:56:08 +0200

    se crea un observable con los roles para que se muestren o escondan los elementos según el rol de los usuarios

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from "@angular/fire/auth";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { UserRoles } from "../model/user-roles";
 
 @Injectable({
   providedIn: "root",
@@ -13,6 +14,8 @@ export class UserService {
   isLoggedOut$: Observable<boolean>;
 
   pictureUrl$: Observable<string>;
+
+  roles$: Observable<UserRoles>;
 
   constructor(private afAuth: AngularFireAuth, private router: Router) {
     // //para obtener la información del usuario, el JSON Web Token con el "idToken"
@@ -28,6 +31,18 @@ export class UserService {
 
     this.pictureUrl$ = afAuth.authState.pipe(
       map((user) => (user ? user.photoURL : null))
+    );
+
+    //idTokenResult devuelve un objeto con el payload del JWT
+    //idToken devuelve el JWT en formato string, en este caso nos interesa el primero
+    this.roles$ = this.afAuth.idTokenResult.pipe(
+      //se devuelven los custom claims que contienen los roles del usuario
+      //si no contiene ninguno, se setea el valor de "admin" a false
+      map((token) => <any>token?.claims ?? { admin: false })
+
+      //con esto consigues que se muestren o escondan varios elementos de la
+      //aplicación basado en el tipo de rol, pero el nivel de seguridad se hace
+      //en la parte de "firestore.rules", aquí sólo se hace a nivel visual
     );
   }
 
