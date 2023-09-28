ba1ce9738476e831b1e5004e54def898407dea8f
CNLZgft <cristian.lopez@gft.com>
2023-09-28 11:07:59 +0200

    añadido al storage un log para saber cuando se está creando un documento (curso)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-spacing */
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,13 +8,44 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
+// "onRequest" is going to be an endpoint for an htpp get request
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
 //   response.send("Hello from Firebase!");
 // });
+
+// "runWith" sirve para estabelecer parametros de confguracion a la hora de
+// definir las cloud funtions. Si se hace a través de la API de Firebase local
+// hay valores límite, pero que se pueden sobrepasar si se crean las functions
+// desde Google Functions Dashboard a través de la consola de Google Cloud
+export const onAddCourseUpdatePromoCounter = functions
+  .runWith({
+    timeoutSeconds: 300,
+    memory: "1GB",
+  })
+  .firestore.document("courses/{courseId}")
+  // onWrite engloba: onUpdate, onCreate y onDelete
+  // el "snapshot" contendrá los datos que van a ser insertados en la bbdd
+  // (el nuevo documento). "context" contendrá información del documento que
+  // se va a crear, por ejemplo, la variable de la ruta que tendrá el doc
+  // se añade el "async" para trabajar más fácilmente con la función, ya que
+  // devuelve una promesa
+  .onCreate(async (snapshot, context) => {
+    functions.logger.debug(
+      `Running add course trigger for courseId ${context.params.courseId}`
+    );
+  });
+
+// hay que usar el comando "npm run build" para obtener la última
+// versión de la carpeta "functions"
+
+// ddbb trigger is a logic that we want to trigger on the sever
+// side at the level of the Firestore server in response to a
+// ddbb event.
